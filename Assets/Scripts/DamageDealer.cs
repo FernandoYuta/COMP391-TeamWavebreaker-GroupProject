ae8c6c97a9c5296e3d0aeeb286e9767c8598f5e7
Charlie <charlie_bi@hotmail.com>
2021-03-19 18:50:16 -0400

    Adding Health and Damage Scripts

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+
+    [SerializeField] int damage = 50;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+    public void DealDamage(GameObject obj)
+    {
+        obj.GetComponent<Health>().DecreaseHealth(damage);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collision detected");
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            DealDamage(other.gameObject);
+            Debug.Log("Deal Damage has been called");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] public int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecreaseHealth(int number)
+    {
+        health -= number;
+
+    }
+
+    public void IncreaseHealth(int number)
+    {
+        health += number;
+    }
+}
